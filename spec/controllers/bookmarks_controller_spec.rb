97f82f48de7f286f9da19c00d9d43a93f34500b7
Tim Schneider <tim@railslove.com>
2012-10-29 17:31:34 +0100

    moar frontend love

---
+++ spec/controllers/bookmarks_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+describe BookmarksController do
+   describe "POST create" do
+    it "redirects to the feed's url" do
+      #feed  = FactoryGirl.create(:feed)
+      #attrs = FactoryGirl.attributes_for(:bookmark, feed: feed)
+      #post :create, feed_id: feed, bookmark: attrs
+      #expect(response).to redirect_to(feed_url(assigns(:feed)))
+    end
+  end
+end
